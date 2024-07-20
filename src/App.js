a840c80eb2aced979d1972aa0db70cf52346e16b
nagaraj161400 <nagaraj161400@gmail.com>
2024-07-20 13:55:33 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import SortableList from "./components/SortableList";
 
 function App() {
-  return <div className="App">{/*<SortableList />*/}</div>;
+  return (
+    <div className="App">
+      <SortableList />
+    </div>
+  );
 }
 
 export default App;
